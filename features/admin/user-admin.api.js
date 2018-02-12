ebd39dce90bdbe8897f87a38e1fe2fd7e4d5c3fd
Duong Dat <tao@cu8-3.local>
2018-02-12 15:19:12 +0700

    ok

--- features/admin/user-admin.api.js
+++ features/admin/user-admin.api.js
@@ -3,7 +3,7 @@ const [requires, func] = [
   (express, api, wrap, User, checkBasic, config) => {
     let router = express.Router();
 
-    api.get(
+    router.get(
       '/',
       wrap(async (req, res, next) => {
         let registrationCount = await User.count();
@@ -11,6 +11,7 @@ const [requires, func] = [
         let tenLast = await User.find().limit(10).sort({
           _id: -1
         });
+        res.set('Content-Type', 'text/plain');
         res.send(
           `Registrations:\t${registrationCount}\nActivated:\t${activationCount}\nLast 10 emails:\t${tenLast
             .map(u => u.email)
@@ -19,11 +20,7 @@ const [requires, func] = [
       })
     );
 
-    api.use(
-      '/admin',
-      checkBasic(config.admin.name, config.admin.pass),
-      router
-    );
+    api.use('/admin', checkBasic(config.admin.name, config.admin.pass), router);
   }
 ];
 
